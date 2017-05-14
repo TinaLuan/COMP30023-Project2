09f6f371eba50cef69175bfa1cb930c1f3abdf76
TinaLuan <tinaluan9794@gmail.com>
2017-05-14 20:08:14 +1000

    stage C finished

--- Questions.c
+++ Questions.c
@@ -1,3 +1,4 @@
 Questions:
 1. If a client send PING for the second time, should there be a response as well?
-2. the program doesn't work well with \r\n  ????? 
+2. the program doesn't work well with \r\n  ?????
+3. erro msg are BYTE arrays???? how to make it byte array, and make it have len 40
